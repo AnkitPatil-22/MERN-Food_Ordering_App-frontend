acaa20b5289efe47f2111be09d76402b45cedb5b
Ankit Patil <ankitpatil2202@gmail.com>
2026-03-21 15:22:31 +0530

    logout user if accessToken expired

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
 import MainNav from "./MainNav";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 
 const Header = () => {
+    const { getAccessTokenSilently, logout, isAuthenticated } = useAuth0();
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            getAccessTokenSilently().catch((err) => {
+                console.error("Session invalid/expired", err);
+                // This force-clears the 'isAuthenticated' state you are using below
+                logout({ logoutParams: { returnTo: window.location.origin } });
+            });
+        }
+    }, [isAuthenticated, getAccessTokenSilently, logout]);
+
     return (
         <div className="border-b-2 border-b-orange-500 py-6 px-4">
             <div className="container mx-auto flex justify-between items-center">
